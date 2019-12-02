57a4eb06122e52d81257c32682c8abd0d801bf22
Shapoval <shapoval.sergey@mail.ru>
2019-12-03 00:54:14 +0700

    Написали валидацию для ФИО

---
+++ reg.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>5 лабораторная - форма регистрации</title>
+<link href = "style.css" rel = "stylesheet"  type="text/css" media="screen">
+</head>
+<body>
+<div id="content">
+	<form id="jform" method="post">
+		<fieldset>
+			<legend>Форма регистрации</legend>
+			<p>
+				<label for="fullname" class="block">ФИО:</label>
+				<input type="text" name="fullname" id="fullname" />
+			</p>
+		
+			</fieldset>
+	
+		
+	</form>
+</div>
+<script type="text/javascript" src="jquery-1.5.1.min.js" charset="utf-8"></script>
+<script type="text/javascript" src="validation.js" charset="utf-8"></script>
+
+
+</body>
+</html>
